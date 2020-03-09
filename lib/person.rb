afb1d916b84957d1b3c9b1a267c441ddb279a681
Nathaniel Adam <iammanyadams@gmail.com>
2020-03-09 22:09:26 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+  def initialize(name)
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
+  def initialize(job)
+    @job = job`
+  end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
