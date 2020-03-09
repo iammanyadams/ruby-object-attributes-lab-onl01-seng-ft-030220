74f10e713c2d9170241b96721eef45b21f239cbf
Nathaniel Adam <iammanyadams@gmail.com>
2020-03-09 22:14:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,4 +5,20 @@
   end
   def name=(name)
     @name = name
+  end
+  def name
+    @name
+  end
+end
+
+
+  def initialize(job)
+    @job = job
+  end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end
 end
